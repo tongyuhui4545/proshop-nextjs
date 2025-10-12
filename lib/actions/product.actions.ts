64f9da3e6ce6990829d62da493438026dd5b94ad
Sam <tongyuhui4545@gmail.com>
2025-10-12 11:32:20 +1100

    feat: serverless setup

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,13 @@
 'use server';
-import { PrismaClient} from "@prisma/client";
+import {prisma} from '@/db/prisma'
+// import { PrismaClient} from "@prisma/client";
 import { convertToPlainObject } from "../utils";
 import {LATEST_PRODUCTS_LIMIT} from '../constants';
 
 
 // Get latest products
 export async function getLatestProducts() {
-    const prisma = new PrismaClient();
+    // const prisma = new PrismaClient();
 
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
