9eb73f908d7ee115725100fd3ca2a9efe60dd4d9
FatSam <tongyuhui4545@gmail.com>
2025-11-16 12:48:01 +1100

    feat: product page finished

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import ProductPrice from "@/components/shared/product/product-price";
 import { Badge } from "@/components/ui/badge";
 import ProductImages from "@/components/shared/product/product-images";
 import AddToCart from "@/components/shared/product/add-to-cart";
+import {getMyCart} from "@/lib/actions/cart.actions"
 
 const ProductDetailsPage = async (props: {
     params: Promise<{ slug: string }>
@@ -12,8 +13,10 @@ const ProductDetailsPage = async (props: {
     const { slug } = await props.params;
 
     const product = await getProductBySlug(slug);
-
     if (!product) notFound();
+
+    const cart = await getMyCart();
+
     return (
         <>
             <section>
@@ -58,7 +61,9 @@ const ProductDetailsPage = async (props: {
                                 </div>
                                 {product.stock > 0 && (
                                     <div className="flex-center">
-                                        <AddToCart item={{
+                                        <AddToCart 
+                                        cart={cart}
+                                        item={{
                                             productId: product.id,
                                             name: product.name,
                                             slug: product.slug,
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,35 +1,76 @@
 'use client'
 
-import { CartItem } from "@/types";
+import { CartItem, Cart } from "@/types";
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation'
-import { Plus } from 'lucide-react'
-import { addItemToCart } from '@/lib/actions/cart.actions';
+import { Plus, Minus } from 'lucide-react'
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { toast } from 'sonner'
+import { useTransition } from 'react'
+import {Loader} from "lucide-react"
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+
+const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
+
     const router = useRouter();
+    const [isPending, startTransition] = useTransition();
+
+    //Check if item is in cart
+    const existItem = cart && cart.items.find((x) => x.productId === item.productId);
 
     const handleAddToCart = async () => {
-        const res = await addItemToCart(item)
+        startTransition(async () => {
+            const res = await addItemToCart(item);
 
-        if (!res.success) {
-            toast.error(res.message ?? res.message);
-            return
-        }
-
-        //Handle success add to cart
-        toast.success(`${item.name} added to cart`, {
-            action: {
-                label: 'Go To Cart',
-                onClick: () => router.push('/cart')
+            if (!res.success) {
+                toast.error(res.message ?? res.message);
+                return
             }
-        })
 
-        return (
-            <Button className='w-full' type='button' onClick={handleAddToCart}><Plus />Add To Cart</Button>
-        )
+            //Handle success add to cart
+            toast.success(`${item.name} added to cart`, {
+                action: {
+                    label: 'Go To Cart',
+                    onClick: () => router.push('/cart')
+                }
+            });
+        })
     }
+
+    //Handle remove from cart
+    const handleRemoveFromCart = async () => {
+        startTransition(async () => {
+            const res = await removeItemFromCart(item.productId);
+
+            toast(res.message, {
+                description: res.success ? "Success" : "Error",
+            });
+        })
+    }
+
+    return (
+        existItem ? (
+            <div>
+                <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+                    {isPending ? (<Loader className='w-4 h4 animate-spin' />) : (
+                        <Minus className='h4 w-4' />
+                    )}
+                </Button>
+                <span className="px-2">{existItem.qty}</span>
+            </div>
+        ) : (
+            <Button className='w-full' type='button' onClick={handleAddToCart}>
+                {isPending ? (
+                    <Loader className="w-4 h-4 animate-spin" />
+                ) : (
+                    <>
+                        <Plus />Add To Cart
+                    </>
+                )}
+
+            </Button>
+
+        ))
 }
 
 export default AddToCart
