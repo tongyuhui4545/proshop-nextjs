f7d7bd14259f8f13e9038f7c970352c833847018
FatSam <tongyuhui4545@gmail.com>
2026-01-20 20:10:20 +1100

    feat: reassign cart and protected routes

--- auth.ts
+++ auth.ts
@@ -75,6 +75,7 @@ export const config = {
         async jwt({ token, user, trigger, session }: any) {
             //assign user fields to token
             if (user) {
+                token.id = user.id;
                 token.role = user.role
             }
             if (user.name === 'NO_NAME') {
@@ -86,9 +87,50 @@ export const config = {
                     data: { name: token.name }
                 })
             }
+
+            if (trigger === 'signIn' || trigger === 'signUp') {
+                const cookiesObject = await cookies();
+                const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+                if (sessionCartId) {
+                    const sessionCart = await prisma.cart.findFirst({
+                        where: { id: sessionCartId }
+                    });
+
+                    if (sessionCart) {
+                        //Delete current user cart
+                        await prisma.cart.deleteMany({
+                            where: { userId: user.id }
+                        });
+
+                        //Assign new cart
+                        await prisma.cart.update({
+                            where: { id: sessionCart.id },
+                            data: { userId: user.id }
+                        })
+                    }
+                }
+            }
             return token
         },
         authorized({ request, auth }: any) {
+            //Array of regex patterns of paths we want to protect
+            const protectedPaths = [
+                /\/shipping-address/,
+                /\/payment-method/,
+                /\/place-order/,
+                /\/profile/,
+                /\/user\/(.*)/,
+                /\/order\/(.*)/,
+                /\/admin/,
+            ]
+
+            //Get pathname from the req URL object
+            const { pathname } = request.nextUrl;
+
+            //Check if user is not authenticated and accessing a protected route
+            if (!auth && protectedPaths.some(p => p.test(pathname))) return false
+
             // Check for session cart cookie
             if (!request.cookies.get('sessionCartId')) {
                 //Generate new session cart id cookie
